103aad03d1c238db3d36acf26668883e8ed13dcc
Doug Allan <dallan93@outlook.com>
2025-11-25 00:07:23 -0500

    adjusting the json parser to have title and ignore numbers

--- scripts/pdf_to_json.py
+++ scripts/pdf_to_json.py
@@ -11,8 +11,10 @@ def pdf_to_json(pdf_path, json_path):
     for page_num in range(len(reader.pages)):
         page = reader.pages[page_num]
         text = page.extract_text()
+        title = get_title(text)
 
         data.append({
+            'title': title,
             'page_number': page_num + 1,
             'text': text
         })
@@ -20,6 +22,18 @@ def pdf_to_json(pdf_path, json_path):
     with open(json_path, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
     pdf.close()
+
+
+def get_title(text):
+    for line in text.splitlines():
+        clean_line = line.strip()
+        if not clean_line:
+            continue
+        if clean_line.isdigit():
+            continue
+        return clean_line
+    
+    return "Untitled"
 
 
 if __name__ == "__main__":
